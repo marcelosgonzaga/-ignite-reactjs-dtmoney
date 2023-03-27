e94953ac7c0240cfc8234dd4b6219d0bbf3b6523
devgonzaga <marcelosgonzaga@gmail.com>
2023-03-26 23:16:10 -0300

    Atualização 23/03/23

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,9 +5,9 @@ export const GlobalStyle = createGlobalStyle`
         --background: #29292E;
         --gray: #393940;
         --green: #00B37E;
+        --red: #F75A68;
         --black: #29292E;
         --black_button: #323238;
-        --red: #F75A68;
         --text-title: #fff;
         --text-bory: #C4C4CC;
         
@@ -54,4 +54,47 @@ export const GlobalStyle = createGlobalStyle`
         opacity:  0.6;
         cursor: not-allowed;
     }
+
+.react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    
+}
+.react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+}
+
+.react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+        filter: brightness(0.8);
+    }
+
+
+}
+
+
+
  `;
